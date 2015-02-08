0613ee1a20b03f557a0618c5f0f8d58bf9d96b22
yandrew <yandrew@gmail.com>
2015-02-07 21:21:49 -0800

    added rspec test file

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,65 @@
+#User.cars or User.prices gets cars or prices for that instance of user. 
+
+
 get '/' do
-	"homepage"
+	erb :index
 end
+
+post '/' do 
+
+end
+
+get '/login' do
+	erb :login
+end
+
+#authenticates login
+post '/login' do
+
+end
+
+# display register page
+get '/register' do
+	erb :register
+end
+
+# goes to homepage or back to register page
+post '/register' do
+
+end
+
+#logout
+get '/logout' do |variable|
+	session.delete :username
+	redirect '/'
+end
+
+
+
+get '/cars/:id' do |id|
+#shows page of prices for a specific car
+	
+	erb :car
+end
+
+get '/cars/:car_id/prices/new' do |car_id|
+	erb :new_price
+end
+
+#shows page for editing a price. 
+get '/cars/:car_id/prices/:price_id/edit' do |car_id, price_id|
+
+end
+
+#update specific price for car
+put '/cars/:car_id/prices/:price_id/edit' do |car_id, price_id|
+
+end
+
+#deletes a price you've created in the past
+delete '/cars/:car_id/prices/:price_id' do |car_id, price_id|
+	
+end
+
+
+
