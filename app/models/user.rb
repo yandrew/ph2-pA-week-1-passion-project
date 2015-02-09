7880f284ae8668cb16ec17490e52a985484bd481
yandrew <yandrew@gmail.com>
2015-02-08 18:42:48 -0800

    registration and login works, but registration does not redirect to the right page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,26 @@ class User < ActiveRecord::Base
   has_many :cars, through: :prices
 
   validates :email, presence: true
-  validates :email, format: { with: /\A[\w]{2,}@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\z/ }
-  validates :name, presence: true
-  validates :password, presence: true
+  validates :email, uniqueness: { message: "email has already been taken" }
+  validates :email, format: { with: /\A[\w]{2,}@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\z/, message: "Not a valid email" }
+  validates :name, presence: {message: "Must have a name"}
+  validates :password_hash, presence: true
 
+# before_validation :validates_blank_password
+# before_save
  # users.password_hash in the database is a :string
   include BCrypt
 
+  # def pw_blank?
+  #   self.password.blank?
+  # end
+
+  # def validates_blank_password
+  #   errors.add(:password_hash, "Please enter a password") if pw_blank?
+  # end
+
   def password
-    @password ||= Password.new(password_hash)
+      @password ||= Password.new(password_hash) 
   end
 
   def password=(new_password)
@@ -22,5 +33,6 @@ class User < ActiveRecord::Base
   def authenticate(password)
     self.password == password
   end
+
 
 end
